89fdee33aa5de4bd457b8adb18824f3145d7c071
Chandana Dongre <113676441+chandanadongre@users.noreply.github.com>
2024-11-27 23:43:59 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", "./*.{html,js}"], 
+  theme: {
+      extend: {
+          colors: {
+              'light-green': {
+                  600: '#4caf50', 
+                  800: '#388e3c', 
+              },
+          },
+      },
+  },
+  plugins: [],
+};
